1812678404dfb6ad53182f25a128e8cbf07e054f
Parvin Sattorova <nanajon66@gmail.com>
2023-08-03 12:43:22 -0600

    feat: Add getTravelerInfo and checkUserLogin functions.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,25 +7,25 @@ function fetchData(endpoint) {
       return response.json();
     })
     .then(data => {
-      console.log("after the JASON promise resolves in the fetchData function", data); 
+      console.log("after the JASON promise resolves in the fetchData function", data);
       return data;
     })
     .catch(error => {
       console.error(error);
       throw new Error(`An error occurred while fetching ${endpoint}.`);
     });
-}
+};
 
 function fetchTravelers() {
   return fetchData('travelers');
-}
+};
 
 function fetchTrips() {
   return fetchData('trips');
-}
+};
 
 function fetchDestinations() {
   return fetchData('destinations');
-}
+};
 
 export { fetchTravelers, fetchTrips, fetchDestinations };
