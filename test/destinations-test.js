a5c5b83ddd2ea3e2cb297ca977a8acc10e8aab46
Parvin Sattorova <nanajon66@gmail.com>
2023-08-04 19:56:58 -0600

    feat: Add helper functions to get traveler info

--- test/destinations-test.js
+++ test/destinations-test.js
@@ -1,7 +1,7 @@
-import chai from 'chai';
-const expect = chai.expect;
-import destinationsTestData from './destinations-test-data';
-import Destinations from './destinations-test';
+// import chai from 'chai';
+// const expect = chai.expect;
+// import destinationsTestData from './destinations-test-data';
+// import Destinations from './destinations-test';
 
 
 // describe('Destinations', function() {
--- test/trips-test.js
+++ test/trips-test.js
@@ -1,7 +1,7 @@
-import chai from 'chai';
-const expect = chai.expect;
-import tripsTestData from './trips-test-data';
-import Trips from './trips-test';
+// import chai from 'chai';
+// const expect = chai.expect;
+// import tripsTestData from './trips-test-data';
+// import Trips from './trips-test';
 
 // describe('Trips', function() {
 //   let trips;
