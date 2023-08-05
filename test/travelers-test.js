2040583405ca5ce29d61134561e8cbfebf13fe39
Parvin Sattorova <nanajon66@gmail.com>
2023-08-04 19:52:29 -0600

    feat: Add helper functions to get traveler info

--- test/travelers-test.js
+++ test/travelers-test.js
@@ -1,36 +1,7 @@
 import chai from 'chai';
 const expect = chai.expect;
 import travelersTestData from './travelers-test-data';
-import Travelers from '../src/Travelers';
+import Travelers from './travelers-test';
 
-describe('Travelers', function() {
-  let travelers;
-
-  beforeEach(() => {
-    travelers = new Travelers(travelersTestData);
-  });
-
-  it('should be a function', function() {
-    expect(Travelers).to.be.a('function');
-  });
-
-  it('should be an instance of travelers', function() {
-    expect(travelers).to.be.an.instanceof(Travelers);
-  });
-  
-  it('should take in a travelers id', function() {
-    expect(travelers.travelers[0].id).to.equal(1);
-  });
-
-  it('should take in a travelers name', function() {
-    expect(travelers.travelers[0].name).to.equal("Ham Leadbeater");
-  });
-
-  it('should take in a travelers type', function() {
-    expect(travelers.travelers[0].travelerType).to.equal("relaxer");
-  });
-
-  it('should be able to return a travelers info based on their user ID', function() {
-    expect(travelers.getTravelerInfo(1)).to.deep.equal({"id": 1, "name": "Ham Leadbeater", "travelerType": "relaxer"});
-  });
-});
+// describe('Travelers', function() {
+//   let travelers;
--- test/trips-test.js
+++ test/trips-test.js
@@ -1,87 +1,9 @@
 import chai from 'chai';
 const expect = chai.expect;
 import tripsTestData from './trips-test-data';
-import Trips from '../src/Trips';
+import Trips from './trips-test';
 
-describe('Trips', function() {
-  let trips;
+// describe('Trips', function() {
+//   let trips;
 
-  beforeEach(() => {
-    trips = new Trips(tripsTestData);
-  });
-
-  it('should be a function', function() {
-    expect(Trips).to.be.a('function');
-  });
-
-  it('should be an instance of trips', function() {
-    expect(trips).to.be.an.instanceof(Trips);
-  });
   
-  it('should take in a trips id', function() {
-    expect(trips.trips[0].id).to.equal(46);
-  });
-
-  it('should take in a trips user ID', function() {
-    expect(trips.trips[0].userID).to.equal(44);
-  });
-
-  it('should take in the destinationID', function() {
-    expect(trips.trips[0].destinationID).to.equal(33);
-  });
-
-  it('should take in the number of travelers', function() {
-    expect(trips.trips[0].travelers).to.equal(2);
-  });
-
-  it('should take in the date', function() {
-    expect(trips.trips[0].date).to.equal("2020/08/24");
-  });
-
-  it('should take in the trip duration', function() {
-    expect(trips.trips[0].duration).to.equal(11);
-  });
-
-  it('should take in the trip status', function() {
-    expect(trips.trips[0].status).to.equal("approved");
-  });
-
-  it('should take in the trips suggested activities', function() {
-    expect(trips.trips[0].suggestedActivities).to.deep.equal([]);
-  });
-
-  it('should return the past trips for a given user', function() {
-    expect(trips.getPastTrips(44)).to.deep.equal([
-      {
-      "id": 46,
-      "userID": 44,
-      "destinationID": 33,
-      "travelers": 2,
-      "date": "2020/08/24",
-      "duration": 11,
-      "status": "approved",
-      "suggestedActivities": []
-      },
-      {
-      "id": 48,
-      "userID": 44,
-      "destinationID": 14,
-      "travelers": 6,
-      "date": "2021/02/10",
-      "duration": 8,
-      "status": "approved",
-      "suggestedActivities": []
-      },
-      {
-      "id": 68,
-      "userID": 44,
-      "destinationID": 41,
-      "travelers": 6,
-      "date": "2020/09/19",
-      "duration": 14,
-      "status": "approved",
-      "suggestedActivities": []
-      }]);
-  });
-});
-
